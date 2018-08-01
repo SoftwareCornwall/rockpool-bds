3421aee9e1c283b20801dbf7659c1f079b2a4da0
Arron Speake <speake.arron@gmail.com>
2018-08-01 17:05:23 +0100

    updated server logging

--- webapp/entry.js
+++ webapp/entry.js
@@ -1,8 +1,12 @@
 var bodyParser = require("body-parser");
 var express = require("express");
 var path = require("path");
+var util = require('util');
+var fs = require('fs');
 
 var app = express();
+
+var logFile = fs.createWriteStream(__dirname + "/server.log", {flags : "w"});
 
 app.use(bodyParser.urlencoded({
 	extended: false
@@ -11,13 +15,10 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use('*', function(req, res, next) {
-   console.log(Date.now() + ": " + req.originalUrl);
-   next();
-});
-
-app.post("/", function(req, res) {
-   console.log("     POST DATA " + req.body.species);
-   res.send(req.body);
+	var text = req.ip.padEnd(20) + "|" + Date.now() + "|:" + req.originalUrl;
+	console.log(text);
+	logFile.write(text + "\n");
+	next();
 });
 
 app.get("/:a/:b", function(req, res) {
