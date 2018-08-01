5eea9d2eef81ecdd44cc3417a566c1e206c51538
James Vanstone <j.d.vanstone@hotmail.co.uk>
2018-08-01 11:35:59 +0100

    added POST request response, tested on postman from laptop

--- central_web_server/app.js
+++ central_web_server/app.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(express.static('ui_files'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.post('/', function(req, res){
+	res.send('POST request to the homepage')	
+})
+
+app.listen(3000, () => console.log('App listening on port 3000!'))
