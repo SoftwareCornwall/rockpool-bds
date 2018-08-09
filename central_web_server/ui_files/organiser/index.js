e1cf5e4b77933444db1447c56fc133065edab071
Arron Speake <speake.arron@gmail.com>
2018-08-09 15:40:31 +0100

    Basic UI

--- central_web_server/ui_files/organiser/index.js
+++ central_web_server/ui_files/organiser/index.js
@@ -1,1 +1,36 @@
+function footerButton_click() {
+    var tideTime = document.getElementById("tide-time-input").value;
+    var tideHeight = document.getElementById("tide-height-input").value;
+    var location = document.getElementById("locations-list").value;
+    
+    if ((tideHeight.trim() == "") || isNaN(tideHeight)) {
+        alert("Low Tide Height must be a valid number.");
+        return;
+    } else if ((tideHeight < -2.0) || (tideHeight > 10.0)) {
+        alert("Low Tide Height must be within -2.0 and 10.0");
+        return;
+    }
+    
+    localStorage.setItem("tideTime", tideTime);
+    localStorage.setItem("tideHeight", tideHeight);
+    localStorage.setItem("location", location);
+}
 
+function handleLocationsFetch(http) {
+    console.log(http);
+
+    if ((http.readyState != http.DONE) || (http.status != 200)) {
+        return;
+    }
+    
+    var json = JSON.parse(http.responseText);
+    console.log(json);
+}
+
+function initialise() {
+    document.getElementById("footer-button").addEventListener("click", footerButton_click);
+    
+    getData("http://10.24.1.69:3000/api/getLocations", handleLocationsFetch);
+}
+
+window.onload = initialise;
